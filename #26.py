9992dc1bbb0ff92396f87ace38eb63ad01d753b8
keerthana <149101981+keerthanathummala@users.noreply.github.com>
2026-01-08 16:29:51 +0000

    #26.py

---
+++ #26.py
@@ -0,0 +1,14 @@
+'''Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).'''
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[k] = nums[i]
+            k += 1
+    return k
