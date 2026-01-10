c1720ea33783a9043b8ac8450c168390921e3ddb
keerthana <149101981+keerthanathummala@users.noreply.github.com>
2026-01-10 16:25:36 +0000

    #28.py

---
+++ #28.py
@@ -0,0 +1,25 @@
+'''Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.'''
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == "":
+            return 0
+
+        n, m = len(haystack), len(needle)
+
+        for i in range(n - m + 1):
+            if haystack[i:i + m] == needle:
+                return i
+
+        return -1
+            
+                
+
+        
