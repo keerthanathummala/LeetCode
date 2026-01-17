a1262faaa6f85292200ca108594361c3b3262c62
keerthana <149101981+keerthanathummala@users.noreply.github.com>
2026-01-17 05:25:56 +0000

    #83.py

---
+++ #83.py
@@ -0,0 +1,24 @@
+'''Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next  # skip duplicate
+            else:
+                current = current.next
+
+        return head
+       
+        
