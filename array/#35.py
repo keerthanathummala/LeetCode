33d29e673950f3e3c78bfc87fd23dc34991a5fc3
keerthana <149101981+keerthanathummala@users.noreply.github.com>
2026-01-25 13:21:32 +0000

    #35.py

---
+++ array/#35.py
@@ -0,0 +1,44 @@
+'''
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.'''
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()  # Sorting is key to avoiding duplicates and using pointers
+        ans = []
+        
+        for i in range(len(nums) - 2):
+            # Skip the same number to avoid duplicate triplets
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            # Use Two Pointers for the rest of the array
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                
+                if total == 0:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    # Move pointers and skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1 # We need a larger sum
+                else:
+                    right -= 1 # We need a smaller sum
+        return ans
