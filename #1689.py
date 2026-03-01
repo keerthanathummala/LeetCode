ab8fe40dc2b358f20178da4a1028b4b80ffdbcd1
keerthana <149101981+keerthanathummala@users.noreply.github.com>
2026-03-01 09:42:52 +0000

    #1689.py

---
+++ #1689.py
@@ -0,0 +1,33 @@
+'''A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+
+Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+
+ 
+
+Example 1:
+
+Input: n = "32"
+Output: 3
+Explanation: 10 + 11 + 11 = 32
+Example 2:
+
+Input: n = "82734"
+Output: 8
+Example 3:
+
+Input: n = "27346209830709182346"
+Output: 9'''
+'''Hint 1
+Think about if the input was only one digit. Then you need to add up as many ones as the value of this digit.
+Hint 2
+If the input has multiple digits, then you can solve for each digit independently, and merge the answers to form numbers that add up to that input.
+Hint 3
+Thus the answer is equal to the max digit.'''
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+        return int(max(n))
+        
